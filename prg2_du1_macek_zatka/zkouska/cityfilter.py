07672349705429da276819f3d171fea7e8025814
František Macek <frantisek.macek1@gmail.com>
2022-03-21 14:28:53 +0100

    deleted zkouska2

--- prg2_du1_macek_zatka/zkouska/cityfilter.py
+++
@@ -1,7 +0,0 @@
-from PySide2.QtCore import QObject, Signal, Slot, Property, QUrl, QAbstractListModel, QByteArray, QRegExp
-obce_proxy = QtCore.QSortFilterProxyModel()
-obce_proxy.setSourceModel(citylist_model)
-obce_proxy.setFilterRegExp(QRegExp("Adamov", QtCore.Qt.CaseSensitivity.CaseInsensitive,
-                                    QRegExp.FixedString))
-
-                            
--- zkouska/cityfilter.py
+++ zkouska/cityfilter.py
@@ -1,15 +1,15 @@
 from PySide2.QtCore import QObject, Signal, Slot, Property, QUrl, QAbstractListModel, QByteArray, QRegExp
 
 from prg2_du1_macek_zatka.city import VIEW_URL
-obce_proxy = QtCore.QSortFilterProxyModel()
-obce_proxy.setSourceModel(citylist_model)
-obce_proxy.setFilterRegExp(QRegExp("Adamov", QtCore.Qt.CaseSensitivity.CaseInsensitive,
-                                    QRegExp.FixedString))
 
-"""               
-VIEW
-proxy 6
-proxy 5
-...
-model_obec
-"""
+
+kraje_proxy = QtCore.QSortFilterProxyModel()
+kraje_proxy.setSourceModel(obce_model)
+kraje_proxy.setFilterRole(obce_model.Roles.REGION.value) #works with argument 0 and name of settlement as argument on next line
+kraje_proxy.setFilterRegExp(QRegExp("", QtCore.Qt.CaseSensitivity.CaseInsensitive, QRegExp.FixedString))
+
+
+
+
+
+ctxt.setContextProperty("KrajeProxy", kraje_proxy)
